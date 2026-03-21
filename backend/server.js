eaeee70a5584e09b2ea8762fde726fb8bb520e0a
siddharth <project@fathommarineconsultants.com>
2026-03-21 12:18:37 +0530

    final1

--- backend/server.js
+++ backend/server.js
@@ -48,10 +48,3 @@ app.use(async (req, res, next) => {
 app.use("/api/enquiries", enquiryRoutes);
 
 module.exports = app;
-
-if (process.env.VERCEL !== "1") {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
